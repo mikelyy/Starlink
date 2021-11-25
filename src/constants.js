eb12c3a5296b677f1d9edb8e067819b3f667ce64
mikelyy <yuyang_li1@brown.edu>
2021-11-25 02:17:36 -0500

    initial commit

---
+++ src/constants.js
@@ -0,0 +1,10 @@
+
+export const SAT_API_KEY = "GXD2SM-56SD5B-HL4U43-4QF4";
+
+export const STARLINK_CATEGORY = "52";
+
+export const NEARBY_SATELLITE = "rest/v1/satellite/above";
+
+export const WORLD_MAP_URL = "https://unpkg.com/world-atlas@1/world/110m.json";
+
+export const SATELLITE_POSITION_URL = "rest/v1/satellite/positions";
